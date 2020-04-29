3cee1e013a7b482cbbe758974b7dd8ed6dc83fa7
Rafael <rafaelhop@hotmail.com>
2020-04-29 19:44:39 -0300

    Add method getPostById

--- src/main/java/com/spring/codeBlog/controller/PostController.java
+++ src/main/java/com/spring/codeBlog/controller/PostController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -17,11 +18,19 @@ public class PostController {
 	@Autowired
 	private PostServiceImpl postService;
 
-	@RequestMapping(value ="/post", method = RequestMethod.GET)
+	@RequestMapping(value ="/posts", method = RequestMethod.GET)
 	public ModelAndView getPosts() {
 		ModelAndView modelAndView = new ModelAndView("post");
 		List<Post> posts = postService.findAll();
 		modelAndView.addObject("posts", posts);
+		return modelAndView;
+	}
+	
+	@RequestMapping(value = "/post/{id}", method = RequestMethod.GET)
+	public ModelAndView getPostById(@PathVariable("id") Long id) {
+		ModelAndView modelAndView = new ModelAndView("postDetails");
+		Post post = postService.findById(id);
+		modelAndView.addObject("post", post);
 		return modelAndView;
 		
 	}
